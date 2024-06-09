8b57eec292b3bad7db9b6f5ac67f25e27d1a0b68
Micaiah Parker <frompinewood@icloud.com>
2024-06-09 17:48:33 -0400

    refactored func names

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJECT = ec
 PROJECT_DESCRIPTION = ANSI terminal string constructor 
-PROJECT_VERSION = 0.1.0
+PROJECT_VERSION = 0.1.1
 
 include erlang.mk
--- src/ec.erl
+++ src/ec.erl
@@ -1,148 +1,148 @@
 -module(ec).
 
--export(['ESC'/0, 'ESC'/1, 'HOME'/0, 'MV'/2, 'RIGHT'/1, 'UP'/1, 'LEFT'/1, 'DOWN'/1,
-         'NEXT'/1, 'PREV'/1, 'COL'/1, 'POS'/0]).
--export(['ERASE_IN_DISPLAY'/0, 'ERASE_CURSOR_END_SCREEN'/0, 'ERASE_CURSOR_BEGIN_SCREEN'/0,
-         'ERASE_SCREEN'/0]).
--export(['ERASE_IN_LINE'/0, 'ERASE_CURSOR_END_LINE'/0, 'ERASE_CURSOR_BEGIN_LINE'/0,
-         'ERASE_LINE'/0]).
+-export([esc/0, esc/1, home/0, move/2, right/1, up/1, left/1, down/1, next/1, prev/1,
+         col/1, pos/0]).
+-export([erase_in_display/0, erase_cursor_end_screen/0, erase_cursor_begin_screen/0,
+         erase_screen/0]).
+-export([erase_in_line/0, erase_cursor_end_line/0, erase_cursor_begin_line/0,
+         erase_line/0]).
 
 -include_lib("eunit/include/eunit.hrl").
 
--spec 'ESC'() -> 16#1B.
-'ESC'() ->
+-spec esc() -> 16#1B.
+esc() ->
   16#1B.
 
-'ESC'(Terms) ->
-  ['ESC'(), Terms].
+esc(Terms) ->
+  [esc(), Terms].
 
 escape_test() ->
-  ?assertEqual($\e, 'ESC'()).
+  ?assertEqual($\e, esc()).
 
 %% Cursor commands
--spec 'HOME'() -> iodata().
-'HOME'() ->
-  ['ESC'(), $[, $H].
+-spec home() -> iodata().
+home() ->
+  [esc(), $[, $H].
 
 home_test() ->
-  ?assertEqual([27, $[, $H], lists:flatten('HOME'())).
+  ?assertEqual([27, $[, $H], lists:flatten(home())).
 
--spec 'MV'(non_neg_integer(), non_neg_integer()) -> iodata().
-'MV'(X, Y) ->
-  'ESC'([$[, integer_to_list(Y), $;, integer_to_list(X), $H]).
+-spec move(non_neg_integer(), non_neg_integer()) -> iodata().
+move(X, Y) ->
+  esc([$[, integer_to_list(Y), $;, integer_to_list(X), $H]).
 
-mv_test() ->
-  ?assertEqual([27, $[, $2, $;, $1, $0, $H], lists:flatten('MV'(10, 2))).
+move_test() ->
+  ?assertEqual([27, $[, $2, $;, $1, $0, $H], lists:flatten(move(10, 2))).
 
--spec 'UP'(non_neg_integer()) -> iodata().
-'UP'(A) ->
-  'ESC'([$[, integer_to_list(A), $A]).
+-spec up(non_neg_integer()) -> iodata().
+up(A) ->
+  esc([$[, integer_to_list(A), $A]).
 
 up_test() ->
-  ?assertEqual([27, $[, $1, $0, $A], lists:flatten('UP'(10))).
+  ?assertEqual([27, $[, $1, $0, $A], lists:flatten(up(10))).
 
--spec 'DOWN'(non_neg_integer()) -> iodata().
-'DOWN'(A) ->
-  'ESC'([$[, integer_to_list(A), $B]).
+-spec down(non_neg_integer()) -> iodata().
+down(A) ->
+  esc([$[, integer_to_list(A), $B]).
 
 down_test() ->
-  ?assertEqual([27, $[, $1, $0, $B], lists:flatten('DOWN'(10))).
+  ?assertEqual([27, $[, $1, $0, $B], lists:flatten(down(10))).
 
--spec 'LEFT'(non_neg_integer()) -> iodata().
-'LEFT'(A) ->
-  'ESC'([$[, integer_to_list(A), $C]).
+-spec left(non_neg_integer()) -> iodata().
+left(A) ->
+  esc([$[, integer_to_list(A), $C]).
 
 left_test() ->
-  ?assertEqual([27, $[, $1, $0, $C], lists:flatten('LEFT'(10))).
+  ?assertEqual([27, $[, $1, $0, $C], lists:flatten(left(10))).
 
--spec 'RIGHT'(non_neg_integer()) -> iodata().
-'RIGHT'(A) ->
-  'ESC'([$[, integer_to_list(A), $D]).
+-spec right(non_neg_integer()) -> iodata().
+right(A) ->
+  esc([$[, integer_to_list(A), $D]).
 
 right_test() ->
-  ?assertEqual([27, $[, $1, $0, $D], lists:flatten('RIGHT'(10))).
+  ?assertEqual([27, $[, $1, $0, $D], lists:flatten(right(10))).
 
--spec 'NEXT'(non_neg_integer()) -> iodata().
-'NEXT'(A) ->
-  'ESC'([$[, integer_to_list(A), $E]).
+-spec next(non_neg_integer()) -> iodata().
+next(A) ->
+  esc([$[, integer_to_list(A), $E]).
 
 next_test() ->
-  ?assertEqual([27, $[, $1, $0, $E], lists:flatten('NEXT'(10))).
+  ?assertEqual([27, $[, $1, $0, $E], lists:flatten(next(10))).
 
--spec 'PREV'(non_neg_integer()) -> iodata().
-'PREV'(A) ->
-  'ESC'([$[, integer_to_list(A), $F]).
+-spec prev(non_neg_integer()) -> iodata().
+prev(A) ->
+  esc([$[, integer_to_list(A), $F]).
 
 prev_test() ->
-  ?assertEqual([27, $[, $1, $0, $F], lists:flatten('PREV'(10))).
+  ?assertEqual([27, $[, $1, $0, $F], lists:flatten(prev(10))).
 
--spec 'COL'(non_neg_integer()) -> iodata().
-'COL'(A) ->
-  'ESC'([$[, integer_to_list(A), $G]).
+-spec col(non_neg_integer()) -> iodata().
+col(A) ->
+  esc([$[, integer_to_list(A), $G]).
 
 col_test() ->
-  ?assertEqual([27, $[, $1, $0, $G], lists:flatten('COL'(10))).
+  ?assertEqual([27, $[, $1, $0, $G], lists:flatten(col(10))).
 
--spec 'POS'() -> iodata().
-'POS'() ->
-  'ESC'([$[, $6, $n]).
+-spec pos() -> iodata().
+pos() ->
+  esc([$[, $6, $n]).
 
 pos_test() ->
-  ?assertEqual([27, $[, $6, $n], lists:flatten('POS'())).
+  ?assertEqual([27, $[, $6, $n], lists:flatten(pos())).
 
 %% Erase
--spec 'ERASE_IN_DISPLAY'() -> iodata().
-'ERASE_IN_DISPLAY'() ->
-  'ESC'([$[, $J]).
+-spec erase_in_display() -> iodata().
+erase_in_display() ->
+  esc([$[, $J]).
 
 erase_in_display_test() ->
-  ?assertEqual([27, $[, $J], lists:flatten('ERASE_IN_DISPLAY'())).
+  ?assertEqual([27, $[, $J], lists:flatten(erase_in_display())).
 
--spec 'ERASE_CURSOR_END_SCREEN'() -> iodata().
-'ERASE_CURSOR_END_SCREEN'() ->
-  'ESC'([$[, $0, $J]).
+-spec erase_cursor_end_screen() -> iodata().
+erase_cursor_end_screen() ->
+  esc([$[, $0, $J]).
 
-erase_cursor_end_test() ->
-  ?assertEqual([27, $[, $0, $J], lists:flatten('ERASE_CURSOR_END_SCREEN'())).
+erase_cursor_end_screen_test() ->
+  ?assertEqual([27, $[, $0, $J], lists:flatten(erase_cursor_end_screen())).
 
--spec 'ERASE_CURSOR_BEGIN_SCREEN'() -> iodata().
-'ERASE_CURSOR_BEGIN_SCREEN'() ->
-  'ESC'([$[, $1, $J]).
+-spec erase_cursor_begin_screen() -> iodata().
+erase_cursor_begin_screen() ->
+  esc([$[, $1, $J]).
 
 erase_cursor_begin_test() ->
-  ?assertEqual([27, $[, $1, $J], lists:flatten('ERASE_CURSOR_BEGIN_SCREEN'())).
+  ?assertEqual([27, $[, $1, $J], lists:flatten(erase_cursor_begin_screen())).
 
--spec 'ERASE_SCREEN'() -> iodata().
-'ERASE_SCREEN'() ->
-  'ESC'([$[, $2, $J]).
+-spec erase_screen() -> iodata().
+erase_screen() ->
+  esc([$[, $2, $J]).
 
 erase_screen_test() ->
-  ?assertEqual([27, $[, $2, $J], lists:flatten('ERASE_SCREEN'())).
+  ?assertEqual([27, $[, $2, $J], lists:flatten(erase_screen())).
 
--spec 'ERASE_IN_LINE'() -> iodata().
-'ERASE_IN_LINE'() ->
-  'ESC'([$[, $K]).
+-spec erase_in_line() -> iodata().
+erase_in_line() ->
+  esc([$[, $K]).
 
 erase_in_line_test() ->
-  ?assertEqual([27, $[, $K], lists:flatten('ERASE_IN_LINE'())).
+  ?assertEqual([27, $[, $K], lists:flatten(erase_in_line())).
 
--spec 'ERASE_CURSOR_END_LINE'() -> iodata().
-'ERASE_CURSOR_END_LINE'() ->
-  'ESC'([$[, $0, $K]).
+-spec erase_cursor_end_line() -> iodata().
+erase_cursor_end_line() ->
+  esc([$[, $0, $K]).
 
 erase_cursor_line_end_test() ->
-  ?assertEqual([27, $[, $0, $K], lists:flatten('ERASE_CURSOR_END_LINE'())).
+  ?assertEqual([27, $[, $0, $K], lists:flatten(erase_cursor_end_line())).
 
--spec 'ERASE_CURSOR_BEGIN_LINE'() -> iodata().
-'ERASE_CURSOR_BEGIN_LINE'() ->
-  'ESC'([$[, $1, $K]).
+-spec erase_cursor_begin_line() -> iodata().
+erase_cursor_begin_line() ->
+  esc([$[, $1, $K]).
 
 erase_cursor_line_begin_test() ->
-  ?assertEqual([27, $[, $1, $K], lists:flatten('ERASE_CURSOR_BEGIN_LINE'())).
+  ?assertEqual([27, $[, $1, $K], lists:flatten(erase_cursor_begin_line())).
 
--spec 'ERASE_LINE'() -> iodata().
-'ERASE_LINE'() ->
-  'ESC'([$[, $2, $K]).
+-spec erase_line() -> iodata().
+erase_line() ->
+  esc([$[, $2, $K]).
 
 erase_line_test() ->
-  ?assertEqual([27, $[, $2, $K], lists:flatten('ERASE_LINE'())).
+  ?assertEqual([27, $[, $2, $K], lists:flatten(erase_line())).
